a54b26c252520ca9ee2fedf28fdae5a66a80b492
xX-TheDoctor-Xx <guilhermesuicida123654@gmail.com>
2016-10-16 22:17:57 +0100

    Added RV UE4 doc nad updated scripts, they now work xD

--- rebuild_ue4.bat
+++ rebuild_ue4.bat
@@ -1,6 +1,7 @@
+@echo off
 call Setup.bat
 call GenerateProjectFiles.bat
 
 call C:\"Program Files (x86)"\"Microsoft Visual Studio 14.0"\Common7\Tools\vsvars32
-call devenv UE4.sln /Rebuild ""Development Editor"|Win64"
+call devenv UE4.sln /Rebuild ""Development Editor"|Win64" /Project UE4
 PAUSE
